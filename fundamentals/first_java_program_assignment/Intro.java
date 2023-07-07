8eca4d461844e6785e57a864e27059b565fbf296
Mitchell Esparza <Alexis75k@icloud.com>
2023-07-06 19:36:16 -0700

    created 2 assignments for my intro to Java

---
+++ fundamentals/first_java_program_assignment/Intro.java
@@ -0,0 +1,7 @@
+public class Intro {
+    public static void main(String[] args) {
+        System.out.println("My name is Mitchell Esparza");
+        System.out.println("I am 9+10 years old");
+        System.out.println("My hometown is Burbank, CA");
+    }
+}
