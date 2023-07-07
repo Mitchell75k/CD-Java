d5a5ed29df6b00b95389bbf3020d71bfcc6ff30a
Mitchell Esparza <Alexis75k@icloud.com>
2023-07-06 21:44:32 -0700

    updated notes assignments, created Cafe.java

---
+++ fundamentals/cafe_java_assignment/Cafe.java
@@ -0,0 +1,47 @@
+
+public class Cafe {
+    public static void main(String[] args) {
+        // APP VARIABLES
+        // Lines of text that will appear in the app. 
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage = ", your order will be ready shortly";
+        String readyMessage = ", your order is ready";
+        String displayTotalMessage = ", your total is $";
+        
+        // Menu variables (add yours below)
+        double mochaPrice = 3.5;
+        Double dripCoffee = 3.5;
+        Double latte = 4.5;
+        Double cappuccino = 5.0;
+        
+        // Customer name variables (add yours below)
+        String customer1 = "Cindhuri";
+        String customer2 = "Jimmy";
+        String customer3 = "Noah";
+        String customer4 = "Sam";
+    
+        // Order completions (add yours below)
+        boolean isReadyOrder1 = false;
+        boolean isReadyOrder2 = true;
+        boolean isReadyOrder3 = false;
+        boolean isReadyOrder4 = true;
+
+        
+        //APP INTERACTION SIMULATION (Add your code for the challenges below)
+        // Example:
+        System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+        // ** Your customer interaction print statements will go here ** //
+        System.out.println(isReadyOrder1 ? customer1 + readyMessage : customer1 + pendingMessage);
+        
+        if (isReadyOrder3) {
+            System.out.println(customer3 + readyMessage);
+            System.out.println(customer3 + displayTotalMessage + cappuccino);
+        } else {
+            System.out.println(customer3 + pendingMessage);
+        }
+
+        System.out.println(isReadyOrder4 ? customer4 + displayTotalMessage + (latte + latte) : customer4 + pendingMessage);
+        
+        System.out.println(customer2 + displayTotalMessage + (latte - dripCoffee));
+    }
+}
--- fundamentals/first_java_program_assignment/Intro.java
+++ fundamentals/first_java_program_assignment/Intro.java
@@ -1,7 +1,13 @@
 public class Intro {
-    public static void main(String[] args) {
+//String[] represents an array of strings, args is the name of the array which can be anything
+    public static void main(String[] args) { 
         System.out.println("My name is Mitchell Esparza");
         System.out.println("I am 9+10 years old");
         System.out.println("My hometown is Burbank, CA");
+        
+        //---------CONDITIONALS------------
+        //creating a ternary operator to make our code more concise
+        boolean isRaining= true;
+        System.out.println(isRaining ? "Wear a raincoat!" : "Sun's out, guns out!");
     }
 }
